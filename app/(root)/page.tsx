8d2ee908c6561a7c387ad33bade5ab3af223a410
samarth-kamble-7 <116360937+samarth-kamble-7@users.noreply.github.com>
2024-04-20 19:25:36 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import Service from "@/components/Services";
-import Offers from "@/components/Feature";
+import Offers from "@/components/Offers";
 import FAQ from "@/components/FAQ";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
