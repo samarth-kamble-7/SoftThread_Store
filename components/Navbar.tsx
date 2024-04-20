b6a33a89201a75837edbb75e355dbe61f02089d8
samarth-kamble-7 <116360937+samarth-kamble-7@users.noreply.github.com>
2024-04-20 16:31:37 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import useCart from "@/lib/hooks/useCart";
-
-import { UserButton, useUser } from "@clerk/nextjs";
-import { CircleUserRound, Menu, Search, ShoppingCart } from "lucide-react";
+import { Menu, Search, ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
+import useCart from "@/lib/hooks/useCart";
 
-const Navbar = () => {
+const NavTest = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { user } = useUser();
@@ -17,106 +17,139 @@ const Navbar = () => {
 
   const [dropdownMenu, setDropdownMenu] = useState(false);
   const [query, setQuery] = useState("");
-
   return (
-    <div className="sticky top-0 z-10 py-2 px-10 flex gap-2 justify-between items-center bg-white max-sm:px-2">
-      <Link href="/">
-        <Image src="/logo.png" alt="logo" width={130} height={100} />
-      </Link>
-
-      <div className="flex gap-4 text-base-bold max-lg:hidden">
-        <Link
-          href="/"
-          className={`hover:text-red-1 ${
-            pathname === "/" && "text-red-1"
-          }`}
-        >
-          Home
-        </Link>
-        <Link
-          href={user ? "/wishlist" : "/sign-in"}
-          className={`hover:text-red-1 ${
-            pathname === "/wishlist" && "text-red-1"
-          }`}
-        >
-          Wishlist
-        </Link>
-        <Link
-          href={user ? "/orders" : "/sign-in"}
-          className={`hover:text-red-1 ${
-            pathname === "/orders" && "text-red-1"
-          }`}
-        >
-          Orders
-        </Link>
-      </div>
-
-      <div className="flex gap-3 border border-grey-2 px-3 py-1 items-center rounded-lg">
-        <input
-          className="outline-none max-sm:max-w-[120px]"
-          placeholder="Search..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button
-          disabled={query === ""}
-          onClick={() => router.push(`/search/${query}`)}
-        >
-          <Search className="cursor-pointer h-4 w-4 hover:text-red-1" />
-        </button>
-      </div>
-
-      <div className="relative flex gap-3 items-center">
-        <Link
-          href="/cart"
-          className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white max-md:hidden"
-        >
-          <ShoppingCart />
-          <p className="text-base-bold">Cart ({cart.cartItems.length})</p>
-        </Link>
-
-        <Menu
-          className="cursor-pointer lg:hidden"
-          onClick={() => setDropdownMenu(!dropdownMenu)}
-        />
-
-        {dropdownMenu && (
-          <div className="absolute top-12 right-5 flex flex-col gap-4 p-3 rounded-lg border bg-white text-base-bold lg:hidden">
-            <Link href="/" className="hover:text-red-1">
-              Home
+    <header className="bg-white fixed w-full">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="md:flex md:items-center md:gap-12">
+            <Link className="block text-teal-600" href="#">
+              <Image src={"/logo.png"} height={150} width={150} alt="Logo" />
             </Link>
-            <Link
-              href={user ? "/wishlist" : "/sign-in"}
-              className="hover:text-red-1"
+          </div>
+
+          <div className="hidden md:block">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <Link
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="/"
+                  >
+                    Home
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="/collections"
+                  >
+                    Collection
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="/products"
+                  >
+                    Product
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-500 transition hover:text-gray-500/75"
+                    href="/wishlist"
+                  >
+                    Wishlist
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex gap-3 border border-grey-2 px-3 py-1 items-center rounded-lg">
+            <input
+              className="outline-none max-sm:max-w-[120px]"
+              placeholder="Search..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button
+              disabled={query === ""}
+              onClick={() => router.push(`/search/${query}`)}
             >
-              Wishlist
-            </Link>
-            <Link
-              href={user ? "/orders" : "/sign-in"}
-              className="hover:text-red-1"
-            >
-              Orders
-            </Link>
+              <Search className="cursor-pointer h-4 w-4 hover:text-red-1" />
+            </button>
+          </div>
+
+          <div className="relative flex gap-3 items-center">
             <Link
               href="/cart"
-              className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
+              className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white max-md:hidden"
             >
               <ShoppingCart />
               <p className="text-base-bold">Cart ({cart.cartItems.length})</p>
             </Link>
-          </div>
-        )}
 
-        {user ? (
-          <UserButton afterSignOutUrl="/sign-in" />
-        ) : (
-          <Link href="/sign-in">
-            <CircleUserRound />
-          </Link>
-        )}
+            <Menu
+              className="cursor-pointer lg:hidden"
+              onClick={() => setDropdownMenu(!dropdownMenu)}
+            />
+            {dropdownMenu && (
+              <div className="absolute top-12 right-5 flex flex-col gap-4 p-3 rounded-lg border bg-white text-base-bold lg:hidden">
+                <Link href="/" className="hover:text-red-1">
+                  Home
+                </Link>
+                <Link
+                  href={user ? "/wishlist" : "/sign-in"}
+                  className="hover:text-red-1"
+                >
+                  Wishlist
+                </Link>
+                <Link
+                  href={user ? "/orders" : "/sign-in"}
+                  className="hover:text-red-1"
+                >
+                  Orders
+                </Link>
+                <Link
+                  href="/cart"
+                  className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
+                >
+                  <ShoppingCart />
+                  <p className="text-base-bold">
+                    Cart ({cart.cartItems.length})
+                  </p>
+                </Link>
+              </div>
+            )}
+            {user ? (
+              <UserButton afterSignOutUrl="/" />
+            ) : (
+              <div className="flex items-center gap-4">
+                <div className="sm:flex sm:gap-4">
+                  <Link
+                    className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                    href="/sign-in"
+                  >
+                    Login
+                  </Link>
+                  <div className="hidden sm:flex">
+                    <Link
+                      className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                      href="/sign-up"
+                    >
+                      Register
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
-export default Navbar;
+export default NavTest;
