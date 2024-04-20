f71ba99b2ea3f590f6d5d16c893d38a0ffff6766
samarth-kamble-7 <116360937+samarth-kamble-7@users.noreply.github.com>
2024-04-20 17:42:47 +0530

    Create page.tsx

---
+++ app/(root)/collections/page.tsx
@@ -0,0 +1,12 @@
+import Collections from "@/components/Collections";
+import React from "react";
+
+const CollectionPage = () => {
+  return (
+    <div className="pt-10">
+      <Collections />
+    </div>
+  );
+};
+
+export default CollectionPage;
