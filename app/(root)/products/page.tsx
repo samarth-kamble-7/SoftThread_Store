6a4569f0b44fdfc0882ee3da3620449633b7a28a
samarth-kamble-7 <116360937+samarth-kamble-7@users.noreply.github.com>
2024-04-20 17:42:19 +0530

    Create page.tsx

---
+++ app/(root)/products/page.tsx
@@ -0,0 +1,12 @@
+import ProductList from "@/components/ProductList";
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="pt-10">
+      <ProductList />
+    </div>
+  );
+};
+
+export default Products;
